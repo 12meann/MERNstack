741d81caaf3f15b0e344559dcc79c12e5845973e
Me-ann Urgel <meann1_2@yahoo.com>
2019-05-27 17:34:44 +0800

    add dotenv

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoURI: "mongodb://meann12:mary123@ds261626.mlab.com:61626/mern_shopping_list"
-}
+  mongoURI: process.env.DATABASE_URL
+};
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const path = require("path")
+const path = require("path");
 const app = express();
+require("dotenv").config();
 
 const items = require("./routes/api/items");
 
@@ -22,18 +23,17 @@ mongoose
     console.log(err);
   });
 
-  //Use routes
+//Use routes
 app.use("/api/items", items);
 
 // serve static assets if in production
 
-if(process.env.NODE_ENV === "production"){
-  app.use(express.static("client/build"))
-  app.get("*", (req, res)=> {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  })
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
-
 
 const port = process.env.PORT || 5000;
 
